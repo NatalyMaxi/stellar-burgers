353aa0926a8f76aa2481bc121681520153a70b88
Natalya Maximova <natka-31@mail.ru>
2025-02-28 20:50:34 +0500

    feat: add navigation

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,23 +1,24 @@
 import { FC } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+
+import { useSelector } from '../../services/store';
+import { selectUser, selectIsAuthTokenChecked } from '@slices';
 import { TPrivateRoute } from './type';
 
 export const PrivateRoute: FC<TPrivateRoute> = ({
   children,
   onlyUnauthorized
 }) => {
-  const isLoggedIn = true;
   const location = useLocation();
-  // Если маршрут только для неавторизованных (onlyUnauthorized = true) и пользователь авторизован,
-  // то перенаправляем его на главную страницу.
-  if (onlyUnauthorized && isLoggedIn) {
+  const user = useSelector(selectUser);
+  const isAuthTokenChecked = useSelector(selectIsAuthTokenChecked);
+
+  if (isAuthTokenChecked && user) {
     const from = location.state?.from || { pathname: '/' };
     return <Navigate to={from} replace />;
   }
 
-  // Если маршрут требует авторизации (onlyUnauthorized = false) и пользователь не авторизован,
-  // то перенаправляем его на страницу входа, сохраняя текущий URL.
-  if (!onlyUnauthorized && !isLoggedIn) {
+  if (!onlyUnauthorized && !user) {
     return <Navigate to='/login' state={{ from: location }} replace />;
   }
 
--- src/services/slices/ordersSlice/index.ts
+++ src/services/slices/ordersSlice/index.ts
@@ -1,5 +1,9 @@
 export {
   ordersSliceReducer,
+  getAllOrdersData,
+  getUserOrders,
+  getOrder,
+  createNewOrder,
   selectOrders,
   selectOrder,
   selectTotalOrders,
